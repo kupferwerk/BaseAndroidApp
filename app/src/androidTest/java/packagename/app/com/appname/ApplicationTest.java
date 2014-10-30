03c496b9e591a03103067446d2ab96d3bd2f7a9c
Janusz Leidgens <leidgens@kupferwerk.net>
2014-10-30 13:25:44 +0100

    initial project

---
+++ app/src/androidTest/java/packagename/app/com/appname/ApplicationTest.java
@@ -0,0 +1,13 @@
+package packagename.app.com.appname;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+   public ApplicationTest() {
+      super(Application.class);
+   }
+}
