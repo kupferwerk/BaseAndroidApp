1b3548bda7b20e8eae818551f38792e04816d447
Janusz Leidgens <leidgens@kupferwerk.net>
2014-11-21 11:25:15 +0100

    added toolbar

--- app/src/main/java/packagename/app/com/appname/core/SplashScreen.java
+++ app/src/main/java/packagename/app/com/appname/core/SplashScreen.java
@@ -2,21 +2,17 @@ package packagename.app.com.appname.core;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 
 import packagename.app.com.appname.R;
 
 public class SplashScreen extends ActionBarActivity {
 
    @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      return super.onCreateOptionsMenu(menu);
-   }
-
-   @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       Injector.inject(this);
       setContentView(R.layout.splash_screen_activity);
+      setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
    }
 }
